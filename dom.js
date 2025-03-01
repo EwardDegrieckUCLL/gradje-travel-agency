f7cb15111c232e4a9c349aaa579db6add561ef18
Eward Degrieck UCLL <eward.degrieck@student.ucll.be>
2025-03-01 16:08:31 +0100

    added Contact page and update styling

--- dom.js
+++ dom.js
@@ -1,9 +1,11 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('nav ul');
 const languageOption = document.querySelector('.language-option');
+const body = document.querySelector('body');
 
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('active');
     navMenu.classList.toggle('active');
     languageOption.classList.toggle('inactive');
+    body.classList.toggle('disable-scrolling');
 });
