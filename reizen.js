5c8fd06ef49c1bdf0ebd010a5f62909a5587d3b4
Eward Degrieck UCLL laptop <eward.degrieck@student.ucll.be>
2025-05-03 14:26:41 +0200

    updated reizen page

---
+++ reizen.js
@@ -0,0 +1,13 @@
+travelList = [
+    {
+        "name": "Zeilweekend",
+        "imgUrl" : "./boxer_sailing.jpg",
+        "pageUrl" : "./reizen/zeilweekend.html",
+        "flagUrl" : "./uk.jpg",
+        "country" : "Verenigd Koninkrijk",
+        "startDate" : "2025-05-09",
+        "endDate" : "2025-05-11",
+        "maxReached" : true
+    }
+]
+
